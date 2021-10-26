25126428c4e58bb5d34367601b7fa965e9619cbd
vagrant <aisle.github@gmail.com>
2021-10-26 07:39:10 +0000

    Refactored assertEqual function so it can compare arrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,18 @@
 const assertEqual = function(actual, expected) {
 
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+    if (Array.isArray(actual)) {
+        for (let i = 0; i < actual.length; i++) {
+            if (actual[i] !== expected[i]) {
+                return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+            } else {
+                return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+            }
+        }
+    }
 
-};
-
+    actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  
+  };
 
 
 assertEqual(1,1);
